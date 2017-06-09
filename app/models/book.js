b71fb43feffa40e0564ad1ccdc4ec9d168ab8161
Simon Han <sh@corp.diamondkey.com>
2017-06-09 15:16:41 +1000

    initial

---
+++ app/models/book.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+//book schema definition
+let BookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    pages: { type: Number, required: true, min: 1 },
+    createdAt: { type: Date, default: Date.now },    
+  }, 
+  { 
+    versionKey: false
+  }
+);
+
+// Sets the createdAt parameter equal to the current time
+BookSchema.pre('save', next => {
+  var now = new Date();
+  if(!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+//Exports the BookSchema for use elsewhere.
+module.exports = mongoose.model('book', BookSchema);
